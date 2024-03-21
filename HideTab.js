bf9b4dadaccaf86ff23a61b2c2f4ff3793d5050c
Girish Uppal <girish.uppal@gmail.com>
2024-03-22 08:58:44 +1000

    Hide Tab JS
    Hide Tab JS for CRM

---
+++ HideTab.js
@@ -0,0 +1,9 @@
+// Hide Tab from model driven app form
+
+// Replace XXX with your model driven app's tab internal name
+
+function HideTab(executionContext)
+{
+    var formContext = executionContext.getFormContext();
+    formContext.ui.tabs.get("XXX").setVisible(false);
+}
